3d4a7979ac6e1af05b5a0a6db2a3ee426753bf98
vmangahis <vince.baron092299@gmail.com>
2025-06-04 03:33:36 +0800

    initial commit

---
+++ Models/Repositories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WyvernWatch.Models
+{
+    public record class Repositories([property: JsonPropertyName("name")] string Name);
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using WyvernWatch.Models;
+using Microsoft.Extensions.Configuration;
+
+ConfigurationBuilder conf = new ConfigurationBuilder();
+IConfiguration iconf = conf.AddUserSecrets<Program>().Build();
+string tk = iconf.GetSection("github")["publicTk"];
+Console.WriteLine(tk);
+
+
+/*using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);*/
+
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    await using Stream stream =
+    await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+    var repositories =
+        await JsonSerializer.DeserializeAsync<List<Repositories>>(stream);
+    
+    foreach(var repo in repositories ?? Enumerable.Empty<Repositories>())
+        Console.WriteLine(repo.Name);
+}
